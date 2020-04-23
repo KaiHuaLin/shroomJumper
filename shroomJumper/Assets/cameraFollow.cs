1fed59cac7eefff99a81049b53a9a5ae98e43cda
kaihualin <kl2cb@mail.missouri.edu>
2020-04-23 00:43:37 -0500

    camera follow

--- shroomJumper/Assets/cameraFollow.cs
+++ shroomJumper/Assets/cameraFollow.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class cameraFollow : MonoBehaviour
 {
+
+    private Transform playerTransform;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
+        Vector3 temp = transform.position;
         
+        temp.x = playerTransform.position.x;
+        
+        transform.position = temp;
     }
 }
