6fef42e7b7750bf464e0a961e94ca5d909fe1559
royaljsanders <royaljsanders@gmail.com>
2020-04-27 19:22:24 -0500

    Changes to motion and control

--- shroomJumper/Assets/Scripts/CharacterControl.cs
+++ shroomJumper/Assets/Scripts/CharacterControl.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class CharacterControl : MonoBehaviour
 {
-    public float speed = 10f;
-    public float jumpStrength = 20f;
+
+    public float walkAccel = 20f; //acceleration while grounded
+    public float jumpAccel = 10f; //acceleration while airborne
+    public float speed = 8f; // max speed
+    public float jumpStrength = 1f; //the height jumped regardless of gravity
+    public float groundDecel = 70f; //deceleration that occurs when no input is made
+
     private bool grounded;
     private CapsuleCollider2D col;
     private Vector2 moveVec;
@@ -22,34 +27,47 @@ public class CharacterControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        moveVec.x = Input.GetAxisRaw("Horizontal") * speed; //x input 
 
-		if (grounded)
-		{
+        float moveInput = Input.GetAxisRaw("Horizontal"); //x input 
+
+        if (grounded)
+        {
             moveVec.y = 0;
 
-            if (Input.GetKey(KeyCode.Space))
+            if (Input.GetKeyDown(KeyCode.Space))
             {
                 moveVec.y = Mathf.Sqrt(2 * jumpStrength * Mathf.Abs(Physics2D.gravity.y));
             }
-		}
+        }
 
-        if (Input.GetKey(KeyCode.Space)&&(!grounded))
+
+        //The corresponding acceleration/deceleration is chosen based on if grounded
+        float accel = grounded ? walkAccel : jumpAccel;
+        float decel = grounded ? groundDecel : 0;
+
+
+
+        if (moveInput != 0)
         {
-            moveVec.y = Mathf.Sqrt(2 * jumpStrength * Mathf.Abs(Physics2D.gravity.y));
+            moveVec.x = Mathf.MoveTowards(moveVec.x, speed * moveInput, (2.5f + (accel * Time.deltaTime)));
+        }
+        else
+        {
+            moveVec.x = Mathf.MoveTowards(moveVec.x, 0, decel * Time.deltaTime);
         }
 
         moveVec.y += Physics2D.gravity.y * Time.deltaTime;
 
-        float speedClamp = Mathf.Sqrt(2 * Mathf.Abs(Physics2D.gravity.y));
-        moveVec.y = Mathf.Clamp(moveVec.y, -speedClamp, speedClamp); //so does constsntly accelerate
-        transform.Translate(moveVec * Time.deltaTime); //moves both axises
+        //float speedClamp = Mathf.Sqrt(4 * Mathf.Abs(Physics2D.gravity.y));
+        //moveVec.y = Mathf.Clamp(moveVec.y, -speedClamp, speedClamp); //so does constsntly accelerate
+
+        transform.Translate(moveVec * Time.deltaTime);
 
         grounded = false;
         /* custom collision detection */
         Collider2D[] collisions = Physics2D.OverlapBoxAll(transform.position, col.size, 0);
-        foreach(Collider2D otherCol in collisions)
-		{
+        foreach (Collider2D otherCol in collisions)
+        {
             if (otherCol == col)
             {
                 continue;
@@ -57,19 +75,19 @@ public class CharacterControl : MonoBehaviour
 
             ColliderDistance2D colliderDistance = otherCol.Distance(col);
 
-			if (colliderDistance.isOverlapped)
-			{
+            if (colliderDistance.isOverlapped)
+            {
                 transform.Translate(colliderDistance.pointA - colliderDistance.pointB);
-                if(Vector2.Angle(colliderDistance.normal, Vector2.up) < 90 && moveVec.y < 0)
-				{
-                    grounded = true; 
-				}
-			}
-            if((colliderDistance.pointA.y < colliderDistance.pointB.y) && moveVec.y > 0)
-			{
+                if (Vector2.Angle(colliderDistance.normal, Vector2.up) < 90 && moveVec.y < 0)
+                {
+                    grounded = true;
+                }
+            }
+            if ((colliderDistance.pointA.y < colliderDistance.pointB.y) && moveVec.y > 0)
+            {
                 moveVec.y = 0;
-			}
-		}
-      	
+            }
+        }
+
     }
 }
