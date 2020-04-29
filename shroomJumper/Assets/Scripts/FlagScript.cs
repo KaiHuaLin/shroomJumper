06aa3cd65524d103c457e518b78e953550f35e86
kaihualin <kl2cb@mail.missouri.edu>
2020-04-29 14:54:06 -0500

    add ending

---
+++ shroomJumper/Assets/Scripts/FlagScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FlagScript : MonoBehaviour
+{
+
+    Scene currentScene;
+    int buildIndex;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentScene = SceneManager.GetActiveScene();
+        buildIndex = currentScene.buildIndex;
+    }
+
+    void OnCollisionEnter2D(Collision2D col){
+        if(col.gameObject.name == "player"){
+            SceneManager.LoadScene(buildIndex + 2);
+        }
+    }
+}
